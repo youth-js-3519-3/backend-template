de4ec47fd723100cd45c36a69169c0fe47d9c5b7
AlexyaCC50 <alexyabbs@gmail.com>
2025-07-24 21:08:35 -0300

    Endpoint para pedido

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { PrismaClient } from "../../generated/prisma";
-import { authMiddleware } from "../middlewares/authMiddlewares";
+import { authMiddleware, CustomRequest } from "../middlewares/authMiddlewares";
 import * as zod from 'zod'
 
 const orderRoutes = Router()
@@ -11,19 +11,144 @@ const OrderProductsDTO = zod.array(zod.object({
     quantity: zod.int()
 }))
 
+const OrderCustomerDTO = zod.object({
+    phone: zod.array(zod.object({
+        country_code: zod.string(),
+        area_code: zod.string(),
+        number: zod.string(),
+    })),
+    address: zod.object({
+        line_1: zod.string(),
+        line_2: zod.string(),
+        zip_code: zod.string(),
+        state: zod.string(),
+        city: zod.string(),
+        country: zod.string(),
+    }),
+    name: zod.string(),
+    email: zod.email(),
+    document: zod.string().max(15),
+})
+
+const OrderPaymentDTO = zod.object({
+    type: zod.enum(['credit_card', 'debit_card']),
+    number: zod.string().length(12),
+    cvv: zod.string().length(3),
+    name: zod.string().optional(),
+    document: zod.string().length(11).optional(),
+    month: zod.int(),
+    year: zod.int(),
+    address: zod.object({
+        line_1: zod.string(),
+        line_2: zod.string(),
+        zip_code: zod.string(),
+        state: zod.string(),
+        city: zod.string(),
+        country: zod.string(),
+    }).optional()
+})
+
 const OrderDTO = zod.object({
     items: OrderProductsDTO,
-    payment: zod.object({
-        type: zod.enum(['credit', 'debit']),
-        number: zod.string().length(12),
-        cvv: zod.string().length(3),
-        name: zod.string(),
-        document: zod.string().length(11)
-    })
+    customer: OrderCustomerDTO,
+    payment: OrderPaymentDTO
 })
 
 orderRoutes.post('/buy', authMiddleware, async (req, res) => {
+    const { body: _body, userInfo } = req as CustomRequest;
+    const body = _body as zod.infer<typeof OrderDTO>
 
+    try {
+        OrderDTO.parse(body)
+    } catch (error) {
+        res.status(400).send({
+            message: "Requisição inválida",
+            details: zod.treeifyError(error as zod.ZodError)
+        })
+        return
+    }
+
+    const itemsIds = body.items
+        .map(({ id }) => id)
+
+    const products = await prisma.product.findMany({
+        where: {
+            id: {
+                in: itemsIds
+            },
+        },
+    })
+
+    const items = body.items.map((item) => {
+        const product = products.find((product) => product.id === item.id)
+        if (!product) {
+            return
+        }
+
+        return {
+            code: product.id,
+            amount: product.price,
+            quantity: item.quantity,
+            description: product.name
+        }
+    })
+
+    const gateway_body = {
+        customer: {
+            name: body.customer.name,
+            document: body.customer.document,
+            email: body.customer.email,
+            type: 'individual',
+            code: userInfo.id,
+            phones: {
+                mobile_phone: body.customer.phone
+            }
+        },
+        items,
+        payments: [
+            {
+                credit_card: {
+                    card: {
+                        billing_address: body.payment.address || body.customer.address,
+                        number: body.payment.number,
+                        holder_name: body.payment.name || body.customer.name,
+                        holder_document: body.payment.document || body.customer.document,
+                        exp_month: body.payment.month,
+                        exp_year: body.payment.year,
+                        cvv: body.payment.cvv
+                    },
+                    operation_type: "auth_and_capture"
+                },
+                payment_method: body.payment.type
+            }
+        ]
+    }
+
+    const secret = Buffer
+        .from(process.env.GATEWAY_API_KEY as string)
+        .toString('base64')
+
+    const request = await fetch('https://api.pagar.me/core/v5/orders', {
+        method: 'post',
+        body: JSON.stringify(gateway_body),
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + secret,
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (!request.ok) {
+        res.status(500).send({
+            message: 'Erro ao fazer o pedido'
+        })
+        return
+    }
+
+    res.send({
+        message: 'Pedido feito',
+        details: {}
+    })
 })
 
 orderRoutes.post('/gateway-confirm', async (req, res) => {
